cffc9a5889444753e52a16b0bc32c6461fbbfd08
EmrullahKutlar <emrullah.ktlr@gmail.com>
2022-04-19 00:27:09 +0300

    ilk dersler

---
+++ 1-GoGiris/1-isimlendirme.go
@@ -0,0 +1,12 @@
+package main
+
+func main() {
+
+}
+
+var name string = "golang"   // Private
+var Version string = "1.0.0" // Public
+/*
+Değişkenin ilk harfi küçük ise diğer packageler erişemez private değişken gibi davranır
+Değşikenin ilk harfi büyük ise public değişken gibi davranır
+*/
---
+++ 1-GoGiris/2-Degiskenler.go
@@ -0,0 +1,41 @@
+package main
+
+func main() {
+	/*
+		var message string
+		message = "Merhaba Go!"
+		var message2 = "Merhaba Go!2"
+		fmt.Println(message)
+		fmt.Println(message2)
+
+		var a, b, c int = 3, 4, 5
+		fmt.Println(a + b + c)
+
+		var c = 3
+		var k, o = "abc", true
+		fmt.Println(c)
+		fmt.Println(k)
+		fmt.Println(o)
+
+		a := 5
+		v, n := "abc", true
+
+		string ifade '' içerisinde tanımlanmaz. Harfin AISECII deki değerini verir
+		a := 'b'
+		println(a)
+
+		var myFloat32 float32 = 44.
+		myComplex := complex(3, 4)
+		println(myFloat32)
+		println(myComplex)
+
+		Bir fonksiyonun dışında (burada main) değişken tanımlarken var, const vs kullanmadan
+		değişkeni tanimlayamayız. Const değişkenleri tanımlarken ilk değeri verilmeli ve bu değer
+		fonksiyon içerisinde değiştirilemez
+
+		var (
+			degisken1 = "asd"
+			degisken2 = 5
+		)
+	*/
+}
